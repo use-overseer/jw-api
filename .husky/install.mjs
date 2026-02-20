7aaf9c29b9bd98156a51fbfcbe22fc42bdc9cafe
@gitgost-anonymous <anonymous@gitgost.local>
2026-02-20 16:19:07 +0000

    ci: run parallel

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -3,4 +3,5 @@ if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
   process.exit(0)
 }
 const husky = (await import('husky')).default
+// eslint-disable-next-line no-console
 console.log(husky())
